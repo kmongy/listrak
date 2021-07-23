717e0fe71ff3727f4703a5981cdbc51a27359b45
Kong Yang <kmongy05@gmail.com>
2021-07-23 12:38:27 -0500

    Updated scope of js logic.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 import '/scss/main.scss';
 
-const mainImg = document.querySelector('#main-img');
 const declineBtn = document.querySelector('.decline-btn');
 const signUpBtn = document.querySelector('.sign-up-btn');
-const emailAddress = document.querySelector('#email-address');
-const emailErrorMessage = document.querySelector('.email-message');
 
 function declineOffer(event) {
   event.preventDefault();
@@ -12,17 +9,21 @@ function declineOffer(event) {
 
 function successClick(event) {
   event.preventDefault();
+
+  let formContainer = document.querySelector('.pop-up-form-container');
+  let successMessage = document.querySelector('.pop-up-success-message');
+  let mainImg = document.querySelector('#main-img');
+
+  let emailAddress = document.querySelector('#email-address');
+  let emailErrorMessage = document.querySelector('.email-message');
   let emailValue = emailAddress.value;
+
   const regexedEmailValue =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (regexedEmailValue.test(emailValue.trim())) {
-    let formContainer = document.querySelector('.pop-up-form-container');
-    let successMessage = document.querySelector('.pop-up-success-message');
-
     mainImg.src = 'imgs/pexels-riciardus-871062.png';
     mainImg.alt = 'Glasses in the sand';
-    emailErrorMessage.classList.remove('error-message');
 
     formContainer.classList.add('success');
     successMessage.classList.add('success');
